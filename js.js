8f5cda64d594e288773fa36c63e68b7ce7759c40
adriano fassina <capdevilaadriano@gmail.com>
2020-09-12 22:53:22 -0300

    Modifique seccion de servicios con parrafo y texto + scroll reveal

--- js.js
+++ js.js
@@ -4,3 +4,6 @@ ScrollReveal().reveal('.acerca-de', { delay: 500 });
 ScrollReveal().reveal('.trabajo', { delay: 500 });
 ScrollReveal().reveal('.descripcion', { delay: 500 });
 ScrollReveal().reveal('.contacto', { delay: 500, origin: 'bottom', });
+ScrollReveal().reveal('.bannersecu', { delay: 500, origin: 'top', });
+ScrollReveal().reveal('.imagen', { delay: 500, origin: 'top', })
+ScrollReveal().reveal('.info-right', { delay: 500, origin: 'top', })
