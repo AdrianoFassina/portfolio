39f4f7bd972842d82348fbe222b15f4c64dfc3a0
adriano fassina <capdevilaadriano@gmail.com>
2020-09-13 22:57:20 -0300

    Agregue seccion con logos de herramientas que utilizamos

--- js.js
+++ js.js
@@ -7,3 +7,13 @@ ScrollReveal().reveal('.contacto', { delay: 500, origin: 'bottom', });
 ScrollReveal().reveal('.bannersecu', { delay: 500, origin: 'top', });
 ScrollReveal().reveal('.imagen', { delay: 500, origin: 'top', })
 ScrollReveal().reveal('.info-right', { delay: 500, origin: 'top', })
+
+$(document).ready(function() {
+    $("#owl-demo").owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        items : 4,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+    });
+  });
+  
